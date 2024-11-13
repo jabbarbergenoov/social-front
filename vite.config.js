77a548b35943bc0698e9f9aa6e57ff4b93a58d4b
bekbolat <bekbolatjabbarbergenov@gmail.com>
2024-11-13 19:28:28 +0500

    reload vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import { sign } from 'crypto';
+import { profile } from 'console';
 
 export default defineConfig({
   root: 'src', // Asosiy papka src
@@ -10,15 +11,14 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'src/index.html'), // Asosiy fayl
         sign: path.resolve(__dirname, 'src/signup.html'), // Asosiy fayl
-        card: path.resolve(__dirname, 'src/card.html'), // Asosiy fayl
+        post: path.resolve(__dirname, 'src/post.html'), // Asosiy fayl
         login: path.resolve(__dirname, 'src/login.html'), // Asosiy fayl
-        reser: path.resolve(__dirname, 'src/reser.html'), // Asosiy fayl
         reser: path.resolve(__dirname, 'src/reser.html'), // Asosiy fayl
         new_post: path.resolve(__dirname, 'src/new-post.html'), // Asosiy fayl
         request: path.resolve(__dirname, 'src/js/request.js'), // Asosiy fayl
-        post: path.resolve(__dirname, 'src/post.html'), // Asosiy fayl
-        post: path.resolve(__dirname, 'src/search.html'), // Asosiy fayl
-        post: path.resolve(__dirname, 'src/js/search.js'), // Asosiy fayl
+        search: path.resolve(__dirname, 'src/search.html'), // Asosiy fayl
+        profile: path.resolve(__dirname, 'src/profile.html'), // Asosiy fayl
+        user: path.resolve(__dirname, 'src/user.html'), // Asosiy fayl
       }
     },
     emptyOutDir: true, // dist papkasini bo'sh qilish
